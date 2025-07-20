84b4f834a6618dfa120ac5517c4b174d36fecc13
ManyToMany <sahar.zs2002@gmail.com>
2025-07-20 16:39:14 +0330

    Managing workout data creating classes

---
+++ other.js
@@ -0,0 +1,4 @@
+// date = (new Date() + '').slice(-10);
+// console.log(date);
+
+console.log(Date.now());
--- script.js
+++ script.js
@@ -12,7 +12,54 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-// App Class
+//////////////////////////////////////////////////////////
+// CLASS WORKOUT
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lng, lat]
+    this.distance = distance; // km
+    this.duration = duration; // min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.calcSpeed;
+  }
+}
+
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cycle1 = new Cycling([39, -12], 27, 95, 523);
+// console.log(run1);
+// console.log(cycle1);
+
+//////////////////////////////////////////////////////////
+// APPLICATION ARCHITECTURE
 class App {
   #map;
   #mapEvent;
@@ -96,3 +143,5 @@ class App {
 }
 
 const app = new App();
+
+// When we start the program, first 'app' will be load and then immediatly the constructor function will be called. So because of this reason we put every function that we want to be run from the object into the constructor. We could write them out of the 'App' class too. But in order to have a better code we wrote them inside the 'App' class.
